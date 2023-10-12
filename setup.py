302abf23e87d123fb3b1d4537e8d719818abe1d5
Daniel <daniel.mohns@posteo.de>
2023-10-12 12:33:40 +0200

    Update PyPI information

--- setup.py
+++ setup.py
@@ -1,14 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-
-setup(
-    name="openpaygo-token", 
-    packages=find_packages(),
-    version='2.2.0',
-    license='Apache 2.0',
-    author="Solaris Offgrid",
-    url='https://github.com/openpaygo/OpenPAYGO-Token',
-    install_requires=[
-        'siphash',
-    ],
-)
+setup()
